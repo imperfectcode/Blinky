a198f6426103f97182154022b7295f937807b5a6
David Robinson <sorddin@facebook.com>
2018-02-08 20:49:34 +1100

    Initial Commit

---
+++ Blinky/BlinkyView.h
@@ -0,0 +1,13 @@
+//
+//  BlinkyView.h
+//  Blinky
+//
+//  Created by David on 8/2/18.
+//  Copyright © 2018 Imperfect Code. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BlinkyView : ScreenSaverView
+
+@end
---
+++ Blinky/BlinkyView.m
@@ -0,0 +1,52 @@
+//
+//  BlinkyView.m
+//  Blinky
+//
+//  Created by David on 8/2/18.
+//  Copyright © 2018 Imperfect Code. All rights reserved.
+//
+
+#import "BlinkyView.h"
+
+@implementation BlinkyView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
