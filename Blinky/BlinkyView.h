eb197b443110882c78461b318f7374f90f09fc7d
David Robinson <drobinson@bedlam.staff.sydney.edu.au>
2018-02-09 12:49:53 +1100

    added "Write a Screen Saver: Part I" tutorial code http://cocoadevcentral.com/articles/000088.php

--- Blinky/BlinkyView.h
+++ Blinky/BlinkyView.h
@@ -5,9 +5,19 @@
 //  Created by David on 8/2/18.
 //  Copyright © 2018 Imperfect Code. All rights reserved.
 //
+//  Based on the tutorial "Write a Screen Saver: Part I" by Brian Christensen
+//  http://cocoadevcentral.com/articles/000088.php
+//
+
 
 #import <ScreenSaver/ScreenSaver.h>
 
 @interface BlinkyView : ScreenSaverView
+{
+    IBOutlet id configSheet;
+    IBOutlet id drawFilledShapesOption;
+    IBOutlet id drawOutlinedShapesOption;
+    IBOutlet id drawBothOption;
+}
 
 @end
